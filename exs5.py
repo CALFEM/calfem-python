bb8348d96bec92494c26fdd52ad36ff106c07fbb
jonas_lindemann <jonas.lindemann@construction.lth.se>
2009-01-26 19:56:34 +0000

    Implementing exs5.

---
+++ exs5.py
@@ -0,0 +1,64 @@
+# example exs5
+# ----------------------------------------------------------------
+# PURPOSE 
+#    Analysis of a simply supported beam.
+# ----------------------------------------------------------------
+
+# REFERENCES
+#     G"oran Sandberg 94-03-08 
+#     Karl-Gunnar Olsson 95-09-28
+#     Ola Dahlblom 2004-09-21
+# ----------------------------------------------------------------
+
+from numpy import *
+from pycalfem import *
+
+# ----- Topology -------------------------------------------------
+
+Edof = array([
+    [1, 2, 3, 4, 5, 6],
+    [4, 5, 6, 7, 8, 9],
+    [7, 8, 9, 10, 11, 12]
+])
+
+# ----- Stiffness matrix K and load vector f ---------------------
+
+K=mat(zeros((12,12)))
+f=mat(zeros((12,1)))
+f[4]=-10000
+
+# ----- Element stiffness matrices  ------------------------------
+
+E=2.1e11
+A=45.3e-4
+I=2510e-8
+ep=array([E A I])
+ex=array([0 3])
+ey=array([0 0])
+
+Ke=beam2e(ex,ey,ep)
+
+# ----- Assemble Ke into K ---------------------------------------
+
+K=assem(Edof,K,Ke);
+
+# ----- Solve the system of equations and compute support forces -
+
+bc=array([1 2 11])
+(a,r)=solveq(K,f,bc);
+
+# ----- Section forces -------------------------------------------
+
+Ed=extract(Edof,a);
+
+es1=beam2s(ex,ey,ep,Ed(1,:));
+es2=beam2s(ex,ey,ep,Ed(2,:));
+es3=beam2s(ex,ey,ep,Ed(3,:));
+
+# ----- Results --------------------------------------------------
+
+print(a)
+print(r)
+print(es1)
+print(es2)
+print(es3)
--- pycalfem.py
+++ pycalfem.py
@@ -118,6 +118,56 @@ def bar2s(ex,ey,ep,ed):
     N=E*A/L*mat([[-1.,1.]])*G*u
     return asscalar(N)
 
+def beam2e(ex,ey,ep,eq=None):
+    """---------------------------------------------------------------------
+        PURPOSE
+         Compute the stiffness matrix for a two dimensional beam element. 
+     
+        INPUT:  ex = [x1 x2]
+                ey = [y1 y2]       element node coordinates
+    
+                ep = [E A I]       element properties
+                                      E: Young's modulus
+                                      A: Cross section area
+                                      I: Moment of inertia
+    
+                eq = [qx qy]       distributed loads, local directions
+     
+        OUTPUT: Ke : element stiffness matrix (6 x 6)
+    
+                fe : element load vector (6 x 1)
+    --------------------------------------------------------------------"""
+
+    b=mat([[ex(2)-ex(1)],[ey(2)-ey(1)]])
+    L = asscalar(sqrt(b.T*b))
+    n=b/L
+    
+    E=ep[0]
+    A=ep[1]
+    I=ep[2]
+    
+    qx=0
+    qy=0
+    if nargin>3; qx=eq(1); qy=eq(2); end
+    
+    Kle=[E*A/L   0            0      -E*A/L      0          0 ;
+           0   12*E*I/L^3   6*E*I/L^2  0   -12*E*I/L^3  6*E*I/L^2;
+           0   6*E*I/L^2    4*E*I/L    0   -6*E*I/L^2   2*E*I/L;
+         -E*A/L  0            0       E*A/L      0          0 ;
+           0   -12*E*I/L^3 -6*E*I/L^2  0   12*E*I/L^3  -6*E*I/L^2;
+           0   6*E*I/L^2    2*E*I/L    0   -6*E*I/L^2   4*E*I/L];
+     
+    fle=L*[qx/2 qy/2 qy*L/12 qx/2 qy/2 -qy*L/12]';
+    
+    G=[n(1) n(2)  0    0    0   0;
+      -n(2) n(1)  0    0    0   0;
+        0    0    1    0    0   0;
+        0    0    0   n(1) n(2) 0;
+        0    0    0  -n(2) n(1) 0;
+        0    0    0    0    0   1];
+    
+    Ke=G'*Kle*G;   fe=G'*fle; 
+
 
 def assem(edof,K,Ke,f=None,fe=None):
     """-------------------------------------------------------------
