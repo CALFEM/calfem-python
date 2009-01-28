0f79e98e822a1b556580dab9d0e9d224dcb1f103
jonas_lindemann <jonas.lindemann@construction.lth.se>
2009-01-28 00:52:37 +0000

    Inital implementation of eldraw2.

--- exs6.py
+++ exs6.py
@@ -12,106 +12,144 @@
 #----------------------------------------------------------------
 
 from numpy import *
+from pycalfem import *
 
-#----- Topology -------------------------------------------------
+# ----- Topology -------------------------------------------------
 
- Edof=[1  4  5  6 1  2  3;
-       2  7  8  9 10 11 12;
-       3  4  5  6  7  8  9];      
+Edof = array([
+    [4,  5,  6, 1,  2,  3],
+    [7,  8,  9, 10, 11, 12],
+    [4,  5,  6,  7,  8,  9]      
+])
 
-#----- Stiffness matrix K and load vector f ---------------------
+# ----- Stiffness matrix K and load vector f ---------------------
 
- K=zeros(12);	f=zeros(12,1);	  f(4)=2e+3;
+K=mat(zeros((12,12)))
+f=mat(zeros((12,1)))
+f[3]=2e+3
 
-#----- Element stiffness and element load matrices  -------------
+# ----- Element stiffness and element load matrices  -------------
 
- E=200e9;
- A1=2e-3;     A2=6e-3;
- I1=1.6e-5;	  I2=5.4e-5;
+E=200e9
+A1=2e-3
+A2=6e-3
+I1=1.6e-5
+I2=5.4e-5
 
+ep1=array([E, A1, I1])
+ep3=array([E, A2, I2])
+ex1=array([0, 0])
+ex2=array([6, 6])
+ex3=array([0, 6])
+ey1=array([4, 0])
+ey2=array([4, 0])
+ey3=array([4, 4])
+eq1=array([0, 0])
+eq2=array([0, 0])
+eq3=array([0, -10e+3])
 
- ep1=[E A1 I1];	 ep3=[E A2 I2];
- ex1=[0 0];      ex2=[6 6];	     ex3=[0 6];
- ey1=[4 0];      ey2=[4 0];	     ey3=[4 4];
- eq1=[0 0];
- eq2=[0 0];
- eq3=[0 -10e+3];
+Ke1=beam2e(ex1,ey1,ep1)
+Ke2=beam2e(ex2,ey2,ep1)
+Ke3,fe3=beam2e(ex3,ey3,ep3,eq3)
 
- Ke1=beam2e(ex1,ey1,ep1);
- Ke2=beam2e(ex2,ey2,ep1);
- [Ke3,fe3]=beam2e(ex3,ey3,ep3,eq3);
+# ----- Assemble Ke into K ---------------------------------------
 
-#----- Assemble Ke into K ---------------------------------------
+assem(Edof[0,:],K,Ke1);
+assem(Edof[1,:],K,Ke2);
+assem(Edof[2,:],K,Ke3,f,fe3);
 
- K=assem(Edof(1,:),K,Ke1);
- K=assem(Edof(2,:),K,Ke2);
- [K,f]=assem(Edof(3,:),K,Ke3,f,fe3);
+# ----- Solve the system of equations and compute reactions ------
 
-#----- Solve the system of equations and compute reactions ------
+bc=array([1,2,3,10,11])
+a,r=solveq(K,f,bc)
 
- bc=[1 0; 2 0; 3 0; 10 0; 11 0];	
- [a,r]=solveq(K,f,bc)
+print "a="
+print a
+print "r="
+print r
 
-#----- Section forces -------------------------------------------
+# ----- Section forces -------------------------------------------
 
- Ed=extract(Edof,a);
+Ed=extract(Edof,a);
 
- es1=beam2s(ex1,ey1,ep1,Ed(1,:),eq1,21) 
- es2=beam2s(ex2,ey2,ep1,Ed(2,:),eq2,21) 
- es3=beam2s(ex3,ey3,ep3,Ed(3,:),eq3,21) 
+es1,ed1,ec1=beam2s(ex1,ey1,ep1,Ed[0,:],eq1,np=21)
+es2,ed2,ec2=beam2s(ex2,ey2,ep1,Ed[1,:],eq2,np=21)
+es3,ed3,ec3=beam2s(ex3,ey3,ep3,Ed[2,:],eq3,np=21)
 
- #----- Draw deformed frame ---------------------------------------
+print "es1="
+print es1
+print "es2="
+print es2
+print "es3="
+print es3
+
+# ----- Draw deformed frame ---------------------------------------
+
+print ex1
+ex = array([
+    ex1,ex2,ex3
+])
+print ex
+ey = array([
+    ey1,ey2,ey3
+])
+print ey
+
+figure()
+#eldraw2(ex1,ey1)
+#eldraw2(ex2,ey2)
+#eldraw2(ex3,ey3)
+eldraw2(ex,ey)
+show()
  
- figure(1)
- plotpar=[2 1 0];
- eldraw2(ex1,ey1,plotpar);
- eldraw2(ex2,ey2,plotpar);
- eldraw2(ex3,ey3,plotpar);
- sfac=scalfact2(ex3,ey3,Ed(3,:),0.1);
- plotpar=[1 2 1];
- eldisp2(ex1,ey1,Ed(1,:),plotpar,sfac);
- eldisp2(ex2,ey2,Ed(2,:),plotpar,sfac);
- eldisp2(ex3,ey3,Ed(3,:),plotpar,sfac);
- axis([-1.5 7.5 -0.5 5.5]); 
- pltscalb2(sfac,[1e-2 0.5 0]);
- axis([-1.5 7.5 -0.5 5.5]);
- title('displacements')
+#figure(1)
+#plotpar=[2 1 0];
+#eldraw2(ex1,ey1,plotpar);
+#eldraw2(ex2,ey2,plotpar);
+#eldraw2(ex3,ey3,plotpar);
+#sfac=scalfact2(ex3,ey3,Ed(3,:),0.1);
+#plotpar=[1 2 1];
+#eldisp2(ex1,ey1,Ed(1,:),plotpar,sfac);
+#eldisp2(ex2,ey2,Ed(2,:),plotpar,sfac);
+#eldisp2(ex3,ey3,Ed(3,:),plotpar,sfac);
+#axis([-1.5 7.5 -0.5 5.5]); 
+#pltscalb2(sfac,[1e-2 0.5 0]);
+#axis([-1.5 7.5 -0.5 5.5]);
+#title('displacements')
  
 #----- Draw normal force diagram --------------------------------
  
- figure(2)
- plotpar=[2 1];
- sfac=scalfact2(ex1,ey1,es1(:,1),0.2);
- eldia2(ex1,ey1,es1(:,1),plotpar,sfac);
- eldia2(ex2,ey2,es2(:,1),plotpar,sfac);
- eldia2(ex3,ey3,es3(:,1),plotpar,sfac);
- axis([-1.5 7.5 -0.5 5.5]);
- pltscalb2(sfac,[3e4 1.5 0]);
- title('normal force')
+#figure(2)
+#plotpar=[2 1];
+#sfac=scalfact2(ex1,ey1,es1(:,1),0.2);
+#eldia2(ex1,ey1,es1(:,1),plotpar,sfac);
+#eldia2(ex2,ey2,es2(:,1),plotpar,sfac);
+#eldia2(ex3,ey3,es3(:,1),plotpar,sfac);
+#axis([-1.5 7.5 -0.5 5.5]);
+#pltscalb2(sfac,[3e4 1.5 0]);
+#title('normal force')
 
 #----- Draw shear force diagram ---------------------------------
  
- figure(3)
- plotpar=[2 1];
- sfac=scalfact2(ex3,ey3,es3(:,2),0.2);
- eldia2(ex1,ey1,es1(:,2),plotpar,sfac);
- eldia2(ex2,ey2,es2(:,2),plotpar,sfac);
- eldia2(ex3,ey3,es3(:,2),plotpar,sfac);
- axis([-1.5 7.5 -0.5 5.5]);
- pltscalb2(sfac,[3e4 0.5 0]);
- title('shear force') 
+#figure(3)
+#plotpar=[2 1];
+#sfac=scalfact2(ex3,ey3,es3(:,2),0.2);
+#eldia2(ex1,ey1,es1(:,2),plotpar,sfac);
+#eldia2(ex2,ey2,es2(:,2),plotpar,sfac);
+#eldia2(ex3,ey3,es3(:,2),plotpar,sfac);
+#axis([-1.5 7.5 -0.5 5.5]);
+#pltscalb2(sfac,[3e4 0.5 0]);
+#title('shear force') 
 
 #----- Draw moment diagram --------------------------------------
  
- figure(4)
- plotpar=[2 1];
- sfac=scalfact2(ex3,ey3,es3(:,3),0.2);
- eldia2(ex1,ey1,es1(:,3),plotpar,sfac);
- eldia2(ex2,ey2,es2(:,3),plotpar,sfac);
- eldia2(ex3,ey3,es3(:,3),plotpar,sfac);
- axis([-1.5 7.5 -0.5 5.5]);
- pltscalb2(sfac,[3e4 0.5 0]);
- title('moment') 
+#figure(4)
+#plotpar=[2 1];
+#sfac=scalfact2(ex3,ey3,es3(:,3),0.2);
+#eldia2(ex1,ey1,es1(:,3),plotpar,sfac);
+#eldia2(ex2,ey2,es2(:,3),plotpar,sfac);
+#eldia2(ex3,ey3,es3(:,3),plotpar,sfac);
+#axis([-1.5 7.5 -0.5 5.5]);
+#pltscalb2(sfac,[3e4 0.5 0]);
+#title('moment') 
 
-#------------------------ end -----------------------------------
- echo off
--- pycalfem.py
+++ pycalfem.py
@@ -1,5 +1,12 @@
 from numpy import *
 
+haveMatplotLib = True
+
+try:
+    from matplotlib.pyplot import *
+except:
+    haveMatplotLib = False
+
 def spring1e(ep):
     """-------------------------------------------------------------
      PURPOSE
@@ -273,7 +280,106 @@ def beam2s(ex,ey,ep,ed,eq=None,np=None):
     es=concatenate((N,V,M),1)
     
     return (es,edi,eci)
+    
+def plante(ex,ey,ep,D,eq=None):
+    """
+     Ke=plante(ex,ey,ep,D)
+     [Ke,fe]=plante(ex,ey,ep,D,eq)
+    -------------------------------------------------------------
+     PURPOSE
+      Calculate the stiffness matrix for a triangular plane stress
+      or plane strain element.
+    
+     INPUT:  ex = [x1 x2 x3]         element coordinates
+             ey = [y1 y2 y3]
+     
+             ep = [ptype t ]         ptype: analysis type
+                                     t: thickness
+     
+             D                       constitutive matrix
+    
+             eq = [bx;               bx: body force x-dir
+                   by]               by: body force y-dir
+    
+     OUTPUT: Ke : element stiffness matrix (6 x 6)
+             fe : equivalent nodal forces (6 x 1)
+    -------------------------------------------------------------
+    
+     LAST MODIFIED: M Ristinmaa 1995-10-25
+     Copyright (c)  Division of Structural Mechanics and
+                    Department of Solid Mechanics.
+                    Lund Institute of Technology
+    -------------------------------------------------------------
+    """
 
+    ptype = ep[0];
+    t = ep[1];
+    
+    bx = 0.0
+    by = 0.0
+    
+    if eq != None:
+        bx = eq[0]
+        by = eq[1]
+        
+    C = matrix(
+        [[1, ex[0], ey[0], 0, 0, 0 ], 
+         [0, 0, 0, 1, ex[0], ey[0] ],
+         [1, ex[1], ey[1], 0, 0, 0 ],
+         [0, 0, 0, 1, ex[1], ey[1] ],
+         [1, ex[2], ey[2], 0, 0, 0 ],
+         [0, 0, 0, 1, ex[2], ey[2] ]]
+        )
+    
+    #print "C =", C
+    
+    A = 0.5*det(matrix([[1, ex[0], ey[0]],[1, ex[1], ey[1]],[1, ex[2], ey[2]]]))
+    
+    #print "A = ", A
+
+    # --------- plane stress --------------------------------------
+    
+    if ptype == 1:
+        B = matrix([[0,1,0,0,0,0],[0,0,0,0,0,1],[0,0,1,0,1,0]])*inv(C)
+        
+        #print "B = ", B
+        
+        colD = D.shape[1]
+        
+        if colD>3:
+            Cm = inv(D)
+            Dm = inv(Cm(ix_((0,1,3),(0,1,3))))
+        else:
+            Dm = D
+            
+        Ke = B.T*Dm*B*A*t
+        fe = A/3*matrix([[bx,by,bx,by,bx,by]]).T*t
+        
+    return Ke, fe
+       
+#%--------- plane strain --------------------------------------       
+#elseif ptype==2
+#       B=[0 1 0 0 0 0
+#          0 0 0 0 0 1
+#          0 0 1 0 1 0]*inv(C);
+#
+#       colD=size(D,2);
+#       if colD>3
+#         Dm=D([1 2 4],[1 2 4]);
+#       else
+#         Dm=D;
+#       end
+#
+#       Ke=B'*Dm*B*A*t;
+#       fe=A/3*[bx by bx by bx by]'*t;
+#       
+#else
+#   error('Error ! Check first argument, ptype=1 or 2 allowed')
+#   return
+#end
+#%--------------------------end--------------------------------
+#
+#
 
 def assem(edof,K,Ke,f=None,fe=None):
     """-------------------------------------------------------------
@@ -437,102 +543,24 @@ def hooke(ptype,E,v):
         
     return D
 
-def plante(ex,ey,ep,D,eq=None):
-    """
-     Ke=plante(ex,ey,ep,D)
-     [Ke,fe]=plante(ex,ey,ep,D,eq)
-    -------------------------------------------------------------
-     PURPOSE
-      Calculate the stiffness matrix for a triangular plane stress
-      or plane strain element.
+def eldraw2(ex,ey,plotpar=None,elnum=None):
     
-     INPUT:  ex = [x1 x2 x3]         element coordinates
-             ey = [y1 y2 y3]
-     
-             ep = [ptype t ]         ptype: analysis type
-                                     t: thickness
-     
-             D                       constitutive matrix
-    
-             eq = [bx;               bx: body force x-dir
-                   by]               by: body force y-dir
-    
-     OUTPUT: Ke : element stiffness matrix (6 x 6)
-             fe : equivalent nodal forces (6 x 1)
-    -------------------------------------------------------------
-    
-     LAST MODIFIED: M Ristinmaa 1995-10-25
-     Copyright (c)  Division of Structural Mechanics and
-                    Department of Solid Mechanics.
-                    Lund Institute of Technology
-    -------------------------------------------------------------
-    """
+    if rank(ex)==1:
+        nen = ex.shape[0]
+        nel = 1
+    else:
+        nen = ex.shape[1]
+        nel = ex.shape[0]
 
-    ptype = ep[0];
-    t = ep[1];
+    xmin = ex.min()
+    xmax = ex.max()
+    xsize = xmax-xmin
+    xmargin = xsize*0.10
+    ymin = ey.min()
+    ymax = ey.max()
+    ysize = ymax-ymin
+    ymargin = ysize*0.10
+    plot(ex.transpose(),ey.transpose())
+    axis([xmin-xmargin,xmax+xmargin,ymin-ymargin,ymax+xmargin])
     
-    bx = 0.0
-    by = 0.0
     
-    if eq != None:
-        bx = eq[0]
-        by = eq[1]
-        
-    C = matrix(
-        [[1, ex[0], ey[0], 0, 0, 0 ], 
-         [0, 0, 0, 1, ex[0], ey[0] ],
-         [1, ex[1], ey[1], 0, 0, 0 ],
-         [0, 0, 0, 1, ex[1], ey[1] ],
-         [1, ex[2], ey[2], 0, 0, 0 ],
-         [0, 0, 0, 1, ex[2], ey[2] ]]
-        )
-    
-    #print "C =", C
-    
-    A = 0.5*det(matrix([[1, ex[0], ey[0]],[1, ex[1], ey[1]],[1, ex[2], ey[2]]]))
-    
-    #print "A = ", A
-
-    # --------- plane stress --------------------------------------
-    
-    if ptype == 1:
-        B = matrix([[0,1,0,0,0,0],[0,0,0,0,0,1],[0,0,1,0,1,0]])*inv(C)
-        
-        #print "B = ", B
-        
-        colD = D.shape[1]
-        
-        if colD>3:
-            Cm = inv(D)
-            Dm = inv(Cm(ix_((0,1,3),(0,1,3))))
-        else:
-            Dm = D
-            
-        Ke = B.T*Dm*B*A*t
-        fe = A/3*matrix([[bx,by,bx,by,bx,by]]).T*t
-        
-    return Ke, fe
-       
-#%--------- plane strain --------------------------------------       
-#elseif ptype==2
-#       B=[0 1 0 0 0 0
-#          0 0 0 0 0 1
-#          0 0 1 0 1 0]*inv(C);
-#
-#       colD=size(D,2);
-#       if colD>3
-#         Dm=D([1 2 4],[1 2 4]);
-#       else
-#         Dm=D;
-#       end
-#
-#       Ke=B'*Dm*B*A*t;
-#       fe=A/3*[bx by bx by bx by]'*t;
-#       
-#else
-#   error('Error ! Check first argument, ptype=1 or 2 allowed')
-#   return
-#end
-#%--------------------------end--------------------------------
-#
-#
