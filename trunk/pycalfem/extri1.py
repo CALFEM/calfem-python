8d459e53296fc993076e1520773257c10f00d8a1
jonas_lindemann <jonas_lindemann@354a6b6f-be2a-4014-a54a-cbd821a6665a>
2009-02-26 19:38:39 +0000

    More work on trimesh2d. Support for multiple dofs per node. Start implementing elements for programming course.

--- trunk/pycalfem/extri1.py
+++ trunk/pycalfem/extri1.py
@@ -7,22 +7,37 @@ from pycalfem_utils import *
 vertices = array([
     [0.0, 0.0],
     [1.0, 0.0],
-    [1.0, 1.0],
+    [1.0, 2.0],
     [0.0, 1.0]
 ])
 
 segments = array([
     [0,1,1],
-    [1,2,1],
-    [2,3,1],
-    [3,0,1]
+    [1,2,2],
+    [2,3,3],
+    [3,0,4]
 ])
 
-coords, bcVert, edof = triangle(vertices, segments, maxArea=0.005)
-dofs = createdofs(coords,1)
+
+coords, edof, dofs, bcVert = trimesh2d(vertices, segments, maxArea=0.01, dofsPerNode=2)
 ex, ey = coordxtr(edof, coords, dofs)
 
-eldraw2(ex, ey)
-show()
+kx = 50
+ky = 50
+t = 1.0
+ep = [t]
 
-#print edof
+D = matrix([
+    [kx, 0.],
+    [0., ky]
+])
+
+Ke = flw2te(ex[0,:],ey[0,:],ep,D)
+print Ke
+
+#eldraw2(ex, ey)
+#show()
+
+
+
+
