048c44569b39d422e0fb5ac00290b591e05aefc8
jonaslindemann <jonas.lindemann@lunarc.lu.se>
2018-03-23 15:44:40 +0100

    More...

--- calfem/core.py
+++ calfem/core.py
@@ -92,8 +92,7 @@ def bar2g(ex,ey,ep,N):
     
     :param list ex: element x coordinates [x1, x2]
     :param list ey: element y coordinates [y1, y2]
-    :param list ep: element properties [E, A], E: Young's modulus, 
-    A: Cross section area
+    :param list ep: element properties [E, A], E: Young's modulus, A: Cross section area
     :param float N: normal force
     :return mat Ke: stiffness matrix [4 x 4]
     """
@@ -135,8 +134,7 @@ def bar2s(ex,ey,ep,ed):
     
     :param list ex: element x coordinates [x1, x2]
     :param list ey: element y coordinates [y1, y2]
-    :param list ep: element properties [E, A], E: Young's modulus, 
-    A: Cross section area
+    :param list ep: element properties [E, A], E: Young's modulus, A: Cross section area
     :param list ed: element displacements [u1, u2, u3, u4]    
     :return float N: element foce [N]    
     """
@@ -166,8 +164,7 @@ def bar3e(ex,ey,ez,ep):
     :param list ex: element x coordinates [x1, x2]
     :param list ey: element y coordinates [y1, y2]
     :param list ez: element z coordinates [z1, z2]
-    :param list ep: element properties [E, A], E: Young's modulus, 
-    A: Cross section area
+    :param list ep: element properties [E, A], E: Young's modulus, A: Cross section area
     :return mat Ke: stiffness matrix, [6 x 6]
     """
     E = ep[0]
@@ -201,8 +198,7 @@ def bar3s(ex,ey,ez,ep,ed):
     :param list ex: element x coordinates [x1, x2]
     :param list ey: element y coordinates [y1, y2]
     :param list ez: element z coordinates [z1, z2]
-    :param list ep: element properties [E, A], E: Young's modulus, 
-    A: Cross section area   
+    :param list ep: element properties [E, A], E: Young's modulus, A: Cross section area   
     :param list ed: element displacements [u1, ..., u6]
     :return float N: normal force
     """
@@ -239,8 +235,7 @@ def beam2e(ex,ey,ep,eq=None):
     
     :param list ex: element x coordinates [x1, x2]
     :param list ey: element y coordinates [y1, y2]
-    :param list ep: element properties [E, A, I], E: Young's modulus, 
-    A: Cross section area, I: Moment of inertia   
+    :param list ep: element properties [E, A, I], E: Young's modulus, A: Cross section area, I: Moment of inertia   
     :param list eq: distributed loads, local directions [qx, qy]
     :return mat Ke: element stiffness matrix [6 x 6]
     :return mat fe: element stiffness matrix [6 x 1] (if eq!=None)
