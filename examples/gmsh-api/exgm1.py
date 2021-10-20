04fa23a53aac83c9ee8464c8ee97ce1799fa641b
jonaslindemann <jonas.lindemann@lunarc.lu.se>
2021-10-20 17:36:57 +0200

    gmsh-api investigation

--- examples/gmsh-api/exgm1.py
+++ examples/gmsh-api/exgm1.py
@@ -4,58 +4,69 @@ import sys
 lc = 0
 
 gmsh.initialize()
-gmsh.model.add("exgmsh1")
-gmsh.model.geo.add_point(0, 0, 0, lc, 1)
-gmsh.model.geo.add_point(.1, 0, 0, lc, 2)
-gmsh.model.geo.add_point(.1, .3, 0, lc, 3)
-p4 = gmsh.model.geo.add_point(0, .3, 0, lc)
-
-gmsh.model.geo.add_line(1, 2, 1)
-gmsh.model.geo.add_line(3, 2, 2)
-gmsh.model.geo.add_line(3, p4, 3)
-gmsh.model.geo.add_line(4, 1, p4)
-
-gmsh.model.geo.add_curve_loop([4, 1, -2, 3], 1)
-gmsh.model.geo.add_plane_surface([1], 1)
-#gmsh.model.geo.mesh.setRecombine(2, 1)
-gmsh.model.geo.synchronize()
+gmsh.model.occ.addBox(0, 0, 0, 1, 1, 1, 1)
+gmsh.model.occ.synchronize()
 
 #gmsh.model.add_physical_group(1, [1, 2, 4], 5)
-ps = gmsh.model.add_physical_group(2, [1])
-gmsh.model.set_physical_name(2, ps, "My surface")
+#ps = gmsh.model.add_physical_group(2, [1])
+#gmsh.model.set_physical_name(2, ps, "My surface")
 
-gmsh.option.setNumber("Mesh.MeshSizeFactor", 0.2)
+#gmsh.option.setNumber("Mesh.MeshSizeFactor", 0.2)
 #gmsh.option.setNumber("Mesh.Algorithm", 6)  # Frontal-Delaunay for 2D meshes
-gmsh.option.setNumber("Mesh.Algorithm", 8) # Frontal-Delaunay for quads
+#gmsh.option.setNumber("Mesh.Algorithm", 8) # Frontal-Delaunay for quads
 #gmsh.model.mesh.setAlgorithm(2, 33, 1)
-gmsh.option.setNumber("Mesh.Smoothing", 100)
-gmsh.option.setNumber("Mesh.RecombineAll", 1)
-gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 2) # or 3
+#gmsh.option.setNumber("Mesh.Smoothing", 100)
+#gmsh.option.setNumber("Mesh.RecombineAll", 1)
+#gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 2) # or 3
 
-gmsh.model.mesh.generate(2)
+gmsh.model.mesh.generate(3)
 
 entities = gmsh.model.get_entities()
 
-for e in entities:
-    dim = e[0]
-    tag = e[1]
-    node_tags, node_coords, node_params = gmsh.model.mesh.get_nodes(dim, tag)
-    elem_types, elem_tags, elem_node_tags = gmsh.model.mesh.get_elements(dim, tag)
+tag = 1
+dim = 3
 
-    type = gmsh.model.getType(e[0], e[1])
-    name = gmsh.model.getEntityName(e[0], e[1])
-    if len(name): name += ' '
-    print("Entity " + name + str(e) + " of type " + type)
+node_tags, node_coords, node_params = gmsh.model.mesh.get_nodes(dim, tag)
+elem_types, elem_tags, elem_node_tags = gmsh.model.mesh.get_elements(dim, tag)
+type = gmsh.model.get_type(dim, tag)
+name = gmsh.model.getEntityName(dim, tag)
 
+print("type =", type)
+print("tag  =", tag)
 
+print("node_tags:", len(node_tags))
+print(node_tags)
+print("node_coords:", len(node_coords))
+print(node_coords)
+print("node_params:", len(node_params))
+print(node_params)
+print("elem_types:", len(elem_types))
+print(elem_types)
+print("elem_tags:", len(elem_tags))
+print(elem_tags[0])
+print("elem_node_tags:", len(elem_node_tags))
+print(elem_node_tags[0])
 
-    num_elem = sum(len(i) for i in elem_tags)
-    print(" - Mesh has " + str(len(node_tags)) + " nodes and " + str(num_elem) +
-          " elements")
+# for e in entities:
+#     dim = e[0]
+#     tag = e[1]
+#     print(dim, tag)
+#     node_tags, node_coords, node_params = gmsh.model.mesh.get_nodes(dim, tag)
+#     elem_types, elem_tags, elem_node_tags = gmsh.model.mesh.get_elements(dim, tag)
 
-    for i in elem_node_tags:
-        print(len(i))
+#     type = gmsh.model.getType(e[0], e[1])
+#     name = gmsh.model.getEntityName(e[0], e[1])
+#     #if len(name): name += ' '
+#     #print("Entity " + name + str(e) + " of type " + type)
 
+#     num_elem = sum(len(i) for i in elem_tags)
+#     #print(" - Mesh has " + str(len(node_tags)) + " nodes and " + str(num_elem) +
+#     #      " elements")
+
+#     for i in elem_node_tags:
+#         print(i)
+
+    
 
     # # * Upward and downward adjacencies:
     # up, down = gmsh.model.get_adjacencies(e[0], e[1])
@@ -87,6 +98,6 @@ for e in entities:
 gmsh.write("exgmsh1.msh")
 
 #if '-nopopup' not in sys.argv:
-#    gmsh.fltk.run()
+gmsh.fltk.run()
 
 gmsh.finalize()
