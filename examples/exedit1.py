3ab7bfcb88741b7310fcb7071c0009a71da678ef
Jonas Lindemann <jonas.lindemann@lunarc.lu.se>
2021-11-15 23:29:48 +0100

    Minor updates
    Updated the comments in the exedit1.py example. Updated license files regarding copyright information.

--- examples/exedit1.py
+++ examples/exedit1.py
@@ -1,4 +1,4 @@
-# -*- coding: iso-8859-15 -*-
+# -*- coding: utf-8 -*-
 
 """
 CALFEM Editor Example
@@ -14,24 +14,26 @@ import calfem.utils as cfu
 import calfem.core as cfc
 import numpy as np
 
-# Creating a square geometry with two markers
+# --- Creating a square geometry with two markers
+
 g = cfg.Geometry()
 
-g.point([0.0, 0.0])  # point 0
+g.point([0.0, 0.0])    # point 0
 g.point([100.0, 0.0])  # point 1
-g.point([100, 100])  # point 2
-g.point([0, 100])  # point 3
+g.point([100, 100])    # point 2
+g.point([0, 100])      # point 3
 
-g.spline([0, 1])  # line 0
-g.spline([1, 2])  # line 1
-g.spline([2, 3])  # line 2
-g.spline([3, 0])  # line 3
+g.spline([0, 1])       # line 0
+g.spline([1, 2])       # line 1
+g.spline([2, 3])       # line 2
+g.spline([3, 0])       # line 3
 
 g.surface([0, 1, 2, 3])  # Connect lines to form surface
 g.setCurveMarker(0, 10)
 g.setCurveMarker(2, 20)
 
-# Open the geometry to allow changes in the CALFEM Geometry Editor
+# --- Open the geometry to allow changes in the CALFEM Geometry Editor
+
 new_geometry, marker_dict = cfe.edit_geometry(g)
 print(marker_dict)
 
@@ -42,11 +44,25 @@ ptype = 1
 ep = [ptype,t]
 D = cfc.hooke(ptype, E, v)
 
-bcs_new = [[marker_dict[10], 0]]  # Every border or point marked with 10 will recieve boundary condition value of 0
-loads_new = [[marker_dict[20], 10e5]]  # Every border or point marked with 20 will recieve load value of 10e5
+# --- Every border or point marked with 10 will recieve boundary 
+# --- condition value of 0
 
-bcs_old = [[10, 0]]  # Every border or point marked with A will recieve boundary condition value of 0
-loads_old = [[20, 10e5]]  # Every border or point marked with B will recieve load value of 10e5
+bcs_new = [[marker_dict[10], 0]]  
+
+# --- Every border or point marked with 20 will recieve load 
+# --- value of 10e5
+
+loads_new = [[marker_dict[20], 10e5]]  
+
+# --- Every border or point marked with A will recieve boundary 
+# --- condition value of 0
+
+bcs_old = [[10, 0]]  
+
+# --- Every border or point marked with B will recieve load 
+# --- value of 10e5
+
+loads_old = [[20, 10e5]]  
 
 el_size_factor = 5
 el_type = 3
@@ -60,12 +76,11 @@ def calc(geometry, bcs, loads, text):
 
     coords, edof, dofs, bdofs, elementmarkers = mesh.create()
 
-    # --- Ber�kna element koordinater
+    # --- Calculate element coordinates
 
     ex, ey = cfc.coordxtr(edof, coords, dofs)
 
-
-    # --- Assemblera systemmatris
+    # --- Assemble system matrix
 
     nDofs = edof.max()
 
@@ -74,7 +89,8 @@ def calc(geometry, bcs, loads, text):
     for eltopo, elx, ely in zip(edof, ex, ey):
         Ke = cfc.planqe(elx, ely, ep, D)
         cfc.assem(eltopo, K, Ke)
-    # --- L�sning av ekvationssystem
+    
+    # --- Solve equation system
 
     f = np.zeros([nDofs, 1])
     bcPrescr = np.array([], int)
@@ -88,24 +104,27 @@ def calc(geometry, bcs, loads, text):
 
     a, r = cfc.solveq(K, f, bcPrescr, bcVal)
 
-    # --- Ber�kna elementkrafter
+    # --- Calculate element forces
 
     ed = cfc.extractEldisp(edof, a)
 
     vonMises = []
 
-    # For each element:
+    # --- For each element:
+
     for i in range(edof.shape[0]):
 
-        # Determine element stresses and strains in the element.
+        # --- Determine element stresses and strains in the element.
+
         es, et = cfc.planqs(ex[i,:], ey[i,:], ep, D, ed[i,:])
 
-        # Calc and append effective stress to list.
+        # --- Calc and append effective stress to list.
         vonMises.append(np.sqrt(np.power(es[0],2) - es[0]*es[1] + np.power(es[1],2) + 3*es[2] ) )
 
     title = "Effective stress" + text
     cfv.draw_element_values(vonMises, coords, edof, mesh.dofs_per_node, mesh.el_type, None, draw_elements=False, draw_undisplaced_mesh=False, title=title)
 
+# --- Display results
 
 cfv.clf()
 calc(g, bcs_old, loads_old, " original")
