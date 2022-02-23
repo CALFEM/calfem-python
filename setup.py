9e4fa9cfe7141089a135007867bb3aab0c9686a6
jonaslindemann <jonas.lindemann@lunarc.lu.se>
2022-02-23 15:54:00 +0100

    Updated requirements.

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'visvis', 'pyvtk', 'matplotlib', 'scipy', 'gmsh', 'qtpy'],
+    install_requires=['numpy', 'visvis', 'pyvtk', 'matplotlib', 'scipy', 'gmsh', 'qtpy', 'vedo'],
     include_package_data=True
     #package_data={'calfem': ['examples/*']}
     
